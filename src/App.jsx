b299437b4d1a1f0068f3434a42e6ecb82fcb3f20
Brisa Martin <brisamartin2001@gmail.com>
2024-06-20 14:50:29 -0300

    primer commit 2da pre entrega react

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import './App.css'
+import Menu  from './components/NavBar/NavBar'
+import ItemListConteiner from './components/Conteiners/ItemListConteiner/itemListConteiner'
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ItemDetailConteiner from "./components/Conteiners/ItemDetailConteiner/itemDetailconteiner";
+
+function App() {
+  
+
+  return (
+    <BrowserRouter>
+
+      <Menu />
+
+      <Routes>
+        <Route path='/' element={<ItemListConteiner />} />
+        <Route path='/category/:idCategory' element={<ItemListConteiner />} />
+        <Route path='/item/:idItem' element={<ItemDetailConteiner />} />
+      </Routes>
+      
+
+    </BrowserRouter>
+    
+  )
+}
+
+export default App
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import './NavBar.css'
+import Cartwidget from '../CartWidget/cartwidget'
+import { Link } from "react-router-dom";
+
+function Menu() {
+
+    return (
+  
+      <nav className="navbar">
+        <Link to={'/'}>
+          <h1>Liluna</h1>
+        </Link>
+        
+        <ul className='nav-ul'>
+
+          <Link to={'/'}>
+            <li className="lista-item">Inicio</li>
+          </Link>
+          <Link to={'/Category/Notbooks'}>
+            <li className="lista-item">Notbooks</li>
+          </Link>
+          <Link to={'/Category/Consolas'}>
+            <li className="lista-item">Consolas</li>
+          </Link>
+          <Link to={'/Category/Auriculares'}>
+            <li className="lista-item">Auriculares</li>
+          </Link>
+  
+          
+
+          <Cartwidget />
+
+        </ul>
+        
+      </nav>
+  
+    );
+  
+  } 
+
+  export default Menu
