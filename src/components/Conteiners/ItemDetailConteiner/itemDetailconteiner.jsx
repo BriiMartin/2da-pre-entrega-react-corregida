1de6e69d9e4f1c518a9d3d1a3ef137be0677d51e
Brisa Martin <brisamartin2001@gmail.com>
2024-07-06 17:32:18 -0300

    commit entrega final

--- src/components/Conteiners/ItemDetailConteiner/itemDetailconteiner.jsx
+++ src/components/Conteiners/ItemDetailConteiner/itemDetailconteiner.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import ItemDetail from '../ItemDetail/itemDetail';
 import { getProductById } from '../../../Data/asyncmonck';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../../../firebase/client'
 
 const ItemDetailConteiner = () => {
   const { idItem } = useParams();
